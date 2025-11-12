b6352dab6c3bac5acf2d567e06023155ce29c54e
darrell379 <arykaran6@gmail.com>
2025-11-11 20:11:13 -0800

    Fix button event listener and update alert message

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// 1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementBYId('sapaButton');
-// 2. Tambahkan 'event listener' untuk aksi 'click'
-TombolSapa.addEventListener('clik', function() (
-    //3. Jalankan kode ini ketika tombol di klik
-    alert('Halo! Terima kasih sudah berkunjung!');
-));
+document.getElementById('sapaButton').addEventListener('click', function() {
+    alert('Halo! Selamat datang di acara Dies Natalis SMAN 2 Ngadiluwih 🎉');
+});
